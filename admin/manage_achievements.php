cff98e259af94eac8b807dcdc945caead7e67c74
Asitara <majsterofdesaster@online.de>
2016-01-02 18:32:43 +0100

    change copyright to 2016

--- admin/manage_achievements.php
+++ admin/manage_achievements.php
@@ -1,9 +1,9 @@
 <?php
 /*	Project:	EQdkp-Plus
- *	Package:	Awards  Plugin
+ *	Package:	Awards Plugin
  *	Link:		http://eqdkp-plus.eu
  *
- *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *	Copyright (C) 2006-2016 EQdkp-Plus Developer Team
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU Affero General Public License as published
